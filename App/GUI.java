633829c311876afb63b4c788dc8dcd82609c52c4
diogo2000neves <61559346+diogo2000neves@users.noreply.github.com>
2021-02-26 11:24:27 +0000

    Removed "HelloWorld"

--- App/GUI.java
+++ App/GUI.java
@@ -1,23 +1,56 @@
 package App;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.BorderFactory;
-import java.awt.GridLayout;
-import java.awt.BorderLayout;
+import javax.swing.*;
+import java.awt.*;
 
-public class GUI {
-    public void openFrame(){
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
+public class GUI extends JFrame{
+    public GUI(){
+        JPanel panel1 = new JPanel(new GridLayout(3, 3));
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+        JPanel panel4 = new JPanel();
+        JPanel panel5 = new JPanel();
 
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(0, 1));
+        JButton button1 = new JButton();
+        JButton button2 = new JButton();
 
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Program");
-        frame.pack();
-        frame.setVisible(true);
+        button1.setText("1");
+        button2.setText("2");
+
+        //panel configuration
+        panel1.setBackground(Color.red);
+        panel2.setBackground(Color.green);
+        panel3.setBackground(Color.blue);
+        panel4.setBackground(Color.yellow);
+        panel5.setBackground(Color.black);
+
+        panel1.setPreferredSize(new Dimension(100,100));
+        panel2.setPreferredSize(new Dimension(100,40));
+        panel3.setPreferredSize(new Dimension(100,40));
+        panel4.setPreferredSize(new Dimension(40,100));
+        panel5.setPreferredSize(new Dimension(40,100));
+
+        GridBagConstraints gc = new GridBagConstraints();
+
+        gc.gridx = 0;
+        gc.gridy = 0;
+        panel1.add(button1, gc);
+        gc.gridx = 1;
+        gc.gridy = 0;
+        panel1.add(button2, gc);
+
+        //initial configurations of the frame
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout(15, 15));
+        this.setSize(new Dimension(700, 400));
+        this.setVisible(true);
+        this.setTitle("Program");
+
+        //other configurations of the frame
+        this.add(panel1, BorderLayout.CENTER);
+        this.add(panel2, BorderLayout.NORTH);
+        this.add(panel3, BorderLayout.SOUTH);
+        this.add(panel4, BorderLayout.WEST);
+        this.add(panel5, BorderLayout.EAST);
     }
 }
